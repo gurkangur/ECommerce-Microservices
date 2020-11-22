7091feb10e8acb04d64c8b971a99bc3093b2ca38
gurkangur <gurkangur154@gmail.com>
2020-11-22 16:34:55 +0300

    Add generic repository

---
+++ src/Catalog/Catalog.Api/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Catalog.Api.Repositories.Interfaces
+{
+    public interface IRepository<T>
+    {
+        Task CreateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> GetWhereAsync(Expression<Func<T, bool>> predicate);
+        Task<T> GetAsync(Guid id);
+    }
+}
---
+++ src/Catalog/Catalog.Api/Repositories/RepositoryBase.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Catalog.Api.Data.Context;
+using Catalog.Api.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.Api.Repositories
+{
+    public class RepositoryBase<T> : IRepository<T> where T : class
+    {
+        protected readonly CatalogDbContext _context;
+        protected RepositoryBase(CatalogDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task CreateAsync(T entity)
+        {
+            _context.Add(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            if (_context.Entry(entity).State == EntityState.Detached)
+                _context.Set<T>().Attach(entity);
+
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Set<T>().Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetWhereAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _context.Set<T>().Where(predicate).ToListAsync();
+        }
+
+        public async Task<T> GetAsync(Guid id)
+        {
+            var entity = await _context.Set<T>().FindAsync(id);
+
+            return entity;
+        }
+
+        private bool _disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            _disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
